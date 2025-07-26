baf040851c27a64f326666a9c1c2eced096b23c3
shoh4n <maffin.killer007@gmail.com>
2025-07-26 16:38:49 +0600

    feat: Add hero banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './Components/Header/Header'
+import Hero from './Components/Hero/Hero'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Hero />
     </>
   )
 }
