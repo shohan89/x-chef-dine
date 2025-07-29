82adceddd5daf53b576c2649ef9fbf946122dcc2
shoh4n <maffin.killer007@gmail.com>
2025-07-29 13:51:08 +0600

    update: change hero text

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ const Hero = () => {
     return (
         <div className="bg-[url(https://i.ibb.co/d0GQgCXK/hero-img.png)] bg-cover bg-center flex flex-col justify-center items-center text-white text-center space-y-4 container mx-auto mt-12 p-12 h-[600px] rounded-[24px]">
             <h2 className="text-5xl font-bold leading-[76px]">Discover an exceptional cooking <br />class tailored for you!</h2>
-            <p className="text-[18px] font-normal leading-8">Learn and Master Basic Programming, Data Structures, Algorithm, OOP, Database and solve 500+ coding <br /> problems to become an exceptionally well world-class Programmer.</p>
+            <p className="text-[18px] font-normal leading-8">In this class, you will learn about some unique style of cooking and kitchen materials.<br/> So, let's join and explore our cooking class now.</p>
             <div className="space-x-4 mt-10">
                 <button className="btn bg-[#0BE58A] text-black px-8 py-5 rounded-full text-[20px] font-semibold cursor-pointer">Explore Now</button>
                 <button className="btn border-white border px-8 py-5 rounded-full text-[20px] font-semibold cursor-pointer">Our Feedback</button>
